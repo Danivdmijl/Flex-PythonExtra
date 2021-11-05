42e6ce1c9f537f87dc6cb62fc6080c28191ef2cd
Danivdmijl <32570@ma-web.nl>
2021-11-05 14:26:31 +0100

    bot

--- 04-Discord/discord_bot.py
+++ 04-Discord/discord_bot.py
@@ -7,9 +7,29 @@ async def on_ready():
     # De bot kan op veel verschillende servers draaien. Met deze regel code pak je de eerste server die deze bot heeft. Als jouw bot maar 1 server heeft is het makkelijk.
     guild = client.guilds[0]
     # De naam van de server printen we gelijk uit.
-    print(guild.name, "is the name of the server")
+    print(guild.name, "is de naam van de server!")
 
     # We printen de naam van de bot user uit.
-    print(client.user, "has connected to the client")
+    print(client.user, "is verbonden met de client")
 
-client.run("OTA2MTY0NjU1NzM3Njg0MDA4.YYUpmQ.LK-tYhALH9ZwXeqztWjoLeJRQhE")
+    channel = guild.text_channels[0]
+    print(channel.name, "is de naam van het kanaal!")
+    await channel.send("Ik ben aanwezig!")
+
+@client.event
+async def on_message(message):
+    print(message.channel.name, "Het bericht is geplaats vanaf dit kanaal.")
+    print(message.content)
+    print(message.author,"is de persoon die het bericht schreef.")
+    print(message.created_at,"is het moment dat het bericht geplaatst is.")
+    print(message.channel,"is het kanaal waar het bericht geplaatst is.")
+
+    await message.channel.send("Hallo " + message.author)
+    if message.author.bot == False:
+        message.channel.send
+
+
+
+
+
+client.run("")
